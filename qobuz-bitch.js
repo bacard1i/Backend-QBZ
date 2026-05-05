5e67dfea3feb19d80448efd9cbef0f7d1436c1b0
bacard1i <Hatestar@icloud.com>
2026-05-05 06:40:04 +0500

    Update qobuz-bitch.js

--- qobuz-bitch.js
+++ qobuz-bitch.js
@@ -1,70 +1,56 @@
-const BACKEND_URL = "https://backend-qbz-production.up.railway.app";
+var BACKEND_URL = "https://backend-qbz-production.up.railway.app";
 
-function fetchWithTimeout(url, timeout) {
+function fetchJSON(url) {
     return new Promise(function(resolve, reject) {
-        var controller = new AbortController();
-        var signal = controller.signal;
-        var timer = setTimeout(function() {
-            controller.abort();
-            reject(new Error("Request timeout"));
-        }, timeout || 15000);
-
-        fetch(url, { signal: signal }).then(function(res) {
-            clearTimeout(timer);
-            if (!res.ok) throw new Error("HTTP " + res.status);
-            return res.json();
-        }).then(function(data) {
-            resolve(data);
-        }).catch(function(err) {
-            clearTimeout(timer);
-            reject(err);
-        });
+        fetch(url)
+            .then(function(res) {
+                if (!res.ok) throw new Error("HTTP " + res.status);
+                return res.json();
+            })
+            .then(function(data) {
+                resolve(data);
+            })
+            .catch(function(err) {
+                console.error(err);
+                reject(err);
+            });
     });
 }
 
-var module = {
+var QobuzBitch = {
     id: "qobuz-bacardii-hires",
     name: "Qobuz’s bitch",
     author: "bacardii",
-    version: "2.2",
-    description: "Strict 24-bit Hi-Res Qobuz (Portugal) - Personal Backend",
+    version: "2.3",
+    description: "Strict 24-bit Hi-Res Qobuz",
 
     searchTracks: function(query, limit) {
-        limit = limit || 25;
+        if (!limit) limit = 25;
         var url = BACKEND_URL + "/8spine/search?q=" + encodeURIComponent(query) + "&limit=" + limit;
         
-        return fetchWithTimeout(url, 15000).then(function(data) {
+        return fetchJSON(url).then(function(data) {
             return { tracks: data.tracks || [], total: data.total || 0 };
-        }).catch(function(e) {
-            console.error(e);
-            return { tracks: [], total: 0 };
         });
     },
 
     getTrackStreamUrl: function(trackId) {
         var url = BACKEND_URL + "/8spine/stream/" + trackId;
         
-        return fetchWithTimeout(url, 15000).then(function(data) {
+        return fetchJSON(url).then(function(data) {
             return {
                 streamUrl: data.streamUrl,
                 track: { audioQuality: "24-bit Hi-Res" }
             };
-        }).catch(function(e) {
-            console.error(e);
-            throw e;
         });
     },
 
     getAlbum: function(albumId) {
         var url = BACKEND_URL + "/8spine/album?album_id=" + albumId;
         
-        return fetchWithTimeout(url, 15000).then(function(data) {
+        return fetchJSON(url).then(function(data) {
             return data;
-        }).catch(function(e) {
-            console.error(e);
-            throw e;
         });
     }
 };
 
-module;
+QobuzBitch;
