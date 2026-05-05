bbcc5fc284c531fdebf0bdb59023e6fa71ce9896
bacard1i <Hatestar@icloud.com>
2026-05-05 07:28:08 +0500

    Update qobuz-bitch.js

--- qobuz-bitch.js
+++ qobuz-bitch.js
@@ -3,9 +3,9 @@ var BACKEND_URL = "https://backend-qbz-production.up.railway.app";
 function fetchJSON(url) {
     return new Promise(function(resolve, reject) {
         fetch(url)
-            .then(function(res) {
-                if (!res.ok) throw new Error("HTTP " + res.status);
-                return res.json();
+            .then(function(r) {
+                if (!r.ok) throw new Error("HTTP " + r.status);
+                return r.json();
             })
             .then(function(data) {
                 resolve(data);
@@ -17,41 +17,43 @@ function fetchJSON(url) {
     });
 }
 
-var QobuzBitch = {
+var searchTracks = function(query, limit) {
+    if (!limit) limit = 25;
+    var url = BACKEND_URL + "/8spine/search?q=" + encodeURIComponent(query) + "&limit=" + limit;
+    
+    return fetchJSON(url).then(function(data) {
+        return { tracks: data.tracks || [], total: data.total || 0 };
+    });
+};
+
+var getTrackStreamUrl = function(trackId) {
+    var url = BACKEND_URL + "/8spine/stream/" + trackId;
+    
+    return fetchJSON(url).then(function(data) {
+        return {
+            streamUrl: data.streamUrl,
+            track: { audioQuality: "24-bit Hi-Res" }
+        };
+    });
+};
+
+var getAlbum = function(albumId) {
+    var url = BACKEND_URL + "/8spine/album?album_id=" + albumId;
+    
+    return fetchJSON(url).then(function(data) {
+        return data;
+    });
+};
+
+return {
     id: "qobuz-bacardii-hires",
     name: "Qobuz’s bitch",
     author: "bacardii",
-    version: "2.4",
-    description: "Strict 24-bit Hi-Res Qobuz via Backend",
-
-    searchTracks: function(query, limit) {
-        if (!limit) limit = 25;
-        var url = BACKEND_URL + "/8spine/search?q=" + encodeURIComponent(query) + "&limit=" + limit;
-        
-        return fetchJSON(url).then(function(data) {
-            return { tracks: data.tracks || [], total: data.total || 0 };
-        });
-    },
-
-    getTrackStreamUrl: function(trackId) {
-        var url = BACKEND_URL + "/8spine/stream/" + trackId;
-        
-        return fetchJSON(url).then(function(data) {
-            return {
-                streamUrl: data.streamUrl,
-                track: { audioQuality: "24-bit Hi-Res" }
-            };
-        });
-    },
-
-    getAlbum: function(albumId) {
-        var url = BACKEND_URL + "/8spine/album?album_id=" + albumId;
-        
-        return fetchJSON(url).then(function(data) {
-            return data;
-        });
-    }
+    version: "2.7",
+    description: "Strict 24-bit Hi-Res Qobuz (Portugal) - Personal Backend",
+    labels: ["QOBUZ", "HI-RES", "24-BIT", "FLAC"],
+    
+    searchTracks: searchTracks,
+    getTrackStreamUrl: getTrackStreamUrl,
+    getAlbum: getAlbum
 };
-
-// MUST be the last line - This is what 8SPINE reads
-QobuzBitch;
