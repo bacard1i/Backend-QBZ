6febf7074a0449ca7c344d33d09871810b3c7c74
bacard1i <Hatestar@icloud.com>
2026-05-08 22:15:47 +0500

    Update qobuz-bitch.js

--- qobuz-bitch.js
+++ qobuz-bitch.js
@@ -1,44 +1,63 @@
 var BACKEND_URL = "https://pushit.hatestar.workers.dev";
 
-var searchTracks = function(query, limit) {
-  if (!limit) limit = 20;
+function fetchJSON(url) {
+    return new Promise(function(resolve, reject) {
+        fetch(url)
+            .then(function(r) {
+                if (!r.ok) throw new Error("HTTP " + r.status);
+                return r.json();
+            })
+            .then(function(data) {
+                resolve(data);
+            })
+            .catch(function(err) {
+                console.error("[Rocks8ar]", err);
+                reject(err);
+            });
+    });
+}
 
-  return fetchJSON(BACKEND_URL + "/search?q=" + encodeURIComponent(query) + "&limit=" + limit)
-    .then(function(data) {
-      return data.tracks || [];
-    })
-    .catch(function() {
-      return [];
+var searchTracks = function(query, limit) {
+    if (!limit) limit = 20;
+
+    var url = BACKEND_URL + "/search?q=" + encodeURIComponent(query) + "&limit=" + limit;
+
+    return fetchJSON(url).then(function(data) {
+        return data.tracks || [];
+    }).catch(function() {
+        return [];
     });
 };
 
 var getTrackStreamUrl = function(trackId) {
-  var url = BACKEND_URL + "/stream/" + trackId;
+    var url = BACKEND_URL + "/stream/" + trackId;
 
-  return fetchJSON(url).then(function(data) {
-    return {
-      streamUrl: data.streamUrl,
-      track: {
-        audioQuality: data.track && data.track.audioQuality ? data.track.audioQuality : "24-bit Hi-Res"
-      }
-    };
-  }).catch(function() {
-    return {
-      streamUrl: null,
-      track: {
-        audioQuality: "Unavailable"
-      }
-    };
-  });
+    return fetchJSON(url).then(function(data) {
+        return {
+            streamUrl: data.streamUrl,
+            track: {
+                audioQuality: data.track && data.track.audioQuality 
+                    ? data.track.audioQuality 
+                    : "24-bit Hi-Res"
+            }
+        };
+    }).catch(function() {
+        return {
+            streamUrl: null,
+            track: {
+                audioQuality: "Unavailable"
+            }
+        };
+    });
 };
 
 return {
-  id: "rocks8ar",
-  name: "Rocks8ar",
-  version: "2.0",
-  author: "bacardii",
-  description: "Qobuz Primary + Direct Tidal Fallback",
+    id: "rocks8ar",
+    name: "Rocks8ar",
+    version: "2.0",
+    author: "bacardii",
+    description: "Qobuz Primary + Direct Tidal Fallback",
 
-  searchTracks: searchTracks,
-  getTrackStreamUrl: getTrackStreamUrl
+    searchTracks: searchTracks,
+    getTrackStreamUrl: getTrackStreamUrl
 };
